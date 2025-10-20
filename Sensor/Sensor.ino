eac74932a73f15d914e2cc51b3cde1c7fcb7e523
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2025-10-20 16:13:22 -0700

    added sound sensor support

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,17 +1,17 @@
 /*This code is designed for an ESP32 
 
   Serial communication will be encoded as follows:
-    Total data sent will be one byte per send. 
-    'X' is irrelevant data. 
-    The four most significant bits will be IR sensor data, with 0xFX being motion detected, and 0x0X being no motion detected.
-    The four least significant bits will be sound sensor data, with 0xXF being sound above a threshold detected, and 0xX0 being sound below the threshold.  */
-
+    Total data sent will be two bytes per send. 
+    First byte is PIR data, second is sound data
+    F is no movement detected, and T is movement detected
+    F is no sound detected, and T is sound detected.  */
 
 
 #include "BluetoothSerial.h"
 
 constexpr int PIRpin = 23;
-String device_name = "Petah";
+constexpr int soundPin = 5;
+String deviceName = "Petah";
 
 // Check if Bluetooth is available
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -24,39 +24,51 @@ String device_name = "Petah";
 #endif
 
 BluetoothSerial SerialBT;
+const char* pw = "9572";
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin(device_name);  //Bluetooth device name
+  SerialBT.begin(deviceName);  //Bluetooth device name
   pinMode(PIRpin, INPUT);
+  pinMode(soundPin, INPUT);
   //SerialBT.deleteAllBondedDevices(); //delete paired devices
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  //SerialBT.setPin(pw, 4);
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", deviceName.c_str());
 }
 
 void loop() {
   static unsigned long time0 = millis();
+  static int8_t soundIsOn = 0;
   unsigned long time1 = millis();
-  static unsigned char PIRState = LOW;
-  unsigned char PIRValue;
-  unsigned char output = 0;
+  static uint8_t PIRState = LOW;
+  uint8_t PIRValue, soundValue;
+  uint8_t outputPIR = 0;
+  uint8_t outputSound = 0;
 
   // send sensor data every second
   if (time1 - time0 > 1000){
     PIRValue = digitalRead(PIRpin);
+    soundValue = digitalRead(soundPin);
 
     if (PIRValue != PIRState){
-      output |= 0xF0;
+      outputPIR = 'T';
       PIRState = PIRValue;
     }
     else {
-      output &= 0x0F;
+      outputPIR = 'F';
       PIRState = PIRValue;
     }
 
-    // here would go the logic for dealing with noise
-    // output |= 0x0F; or output &= 0xF0;
+    if (soundValue == HIGH) {
+      outputSound = 'T';
+    }
+    else {
+      outputSound = 'F';
+    }
 
-    SerialBT.write(output);
+    uint8_t data[] = {outputPIR, outputSound, '\n'};
+    SerialBT.write(data, sizeof(data));
+
     time0 = time1;
   }
 
