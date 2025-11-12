80aebb72cffd301000c220517f7d30f9def653f9
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2025-11-11 21:06:51 -0800

    changed to send data over wifi instead of bluetooth

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,73 +1,94 @@
-/*This code is designed for an ESP32 
-
-  Serial communication will be encoded as follows:
-    Total data sent will be two bytes per send. 
-    First byte is PIR data, second is sound data
-    F is no movement detected, and T is movement detected
-    F is no sound detected, and T is sound detected.  */
-
-
-#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 constexpr int PIRpin = 23;
 constexpr int soundPin = 5;
-String deviceName = "Petah";
 
-// Check if Bluetooth is available
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+const char* ssid = "xxxxx";                                   // change to your ssid
+const char* pw = "xxxxx";                                     // change to your password
 
-// Check Serial Port Profile
-#if !defined(CONFIG_BT_SPP_ENABLED)
-#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
-#endif
-
-BluetoothSerial SerialBT;
-const char* pw = "9572";
+IPAddress serverIP = IPAddress(192, 168, xxx, xxx);           // change to host server ip
+uint16_t port = 8000;
 
 void setup() {
+
   Serial.begin(115200);
-  SerialBT.begin(deviceName);  //Bluetooth device name
+  Serial.setDebugOutput(true);
+  Serial.println();
+
   pinMode(PIRpin, INPUT);
   pinMode(soundPin, INPUT);
-  //SerialBT.deleteAllBondedDevices(); //delete paired devices
-  //SerialBT.setPin(pw, 4);
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", deviceName.c_str());
+  
+  WiFi.begin(ssid, pw);                                       // Wifi setup
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print('.');
+  }
+  
+  Serial.print('\n');
+  Serial.print("WiFi connected. IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   static unsigned long time0 = millis();
-  static int8_t soundIsOn = 0;
   unsigned long time1 = millis();
   static uint8_t PIRState = LOW;
-  uint8_t PIRValue, soundValue;
-  uint8_t outputPIR = 0;
-  uint8_t outputSound = 0;
 
-  // send sensor data every second
-  if (time1 - time0 > 1000){
+ 
+  if (time1 - time0 > 1000){                                  // send sensor data every second
+
+    WiFiClient client;
+
+    uint8_t PIRValue, soundValue;
+    char* outputPIR;
+    char* outputSound;
+    
     PIRValue = digitalRead(PIRpin);
     soundValue = digitalRead(soundPin);
 
     if (PIRValue != PIRState){
-      outputPIR = 'T';
+      outputPIR = "T";
       PIRState = PIRValue;
     }
     else {
-      outputPIR = 'F';
+      outputPIR = "F";
       PIRState = PIRValue;
     }
 
     if (soundValue == HIGH) {
-      outputSound = 'T';
+      outputSound = "T";
     }
     else {
-      outputSound = 'F';
+      outputSound = "F";
     }
 
-    uint8_t data[] = {outputPIR, outputSound, '\n'};
-    SerialBT.write(data, sizeof(data));
+    char output[25];
+    strcpy(output, "{\"Movement\":");
+    strcat(output, outputPIR);
+    strcat(output, ", \"Sound\":");
+    strcat(output, outputSound);
+    strcat(output, "}");
+    
+    Serial.println(output);
+
+    if (client.connect(serverIP, port)){                      //connect and send http request
+      client.println("POST / HTTP/1.1");
+      client.println("Host: 192.168.xxx.xxx");
+      client.println("Content-type: application/json");
+      client.println("Content-length: 25");
+      client.println("Connection: close");
+      client.println();
+      client.println(output);
+
+      Serial.println("Data Sent");
+      client.stop();
+    } 
+    else {
+      Serial.print("Failed to connect to server: ");
+      Serial.printf("time0: %lu, time1: %lu\n", time0, time1);
+    }
 
     time0 = time1;
   }
