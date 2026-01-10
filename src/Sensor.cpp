882c774bfa8f53feef96a9c5e729406b92a4cc8a
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2026-01-09 16:52:19 -0800

    added automatic reconnect to server

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -3,12 +3,13 @@
 
 constexpr uint16_t SOUNDPIN = 36;
 constexpr uint16_t SAMPLES = 1024;
+constexpr uint16_t DIVIDER_VALUE = 80;
 constexpr uint16_t SAMPLE_FREQUENCY = 1000;                   // sample frequency can't be very high due to analogRead limitations
 constexpr uint32_t ALARM_VALUE = 1000000 / SAMPLE_FREQUENCY;  // in microseconds
 
 const char* ssid = "ssid";                                    // change to your ssid
 const char* pw = "pw";                                        // change to your password
-const char* serverHost = "192.168.x.x";                       // change to host server IP
+const char* serverHost = "10.141.xx.x";                       // change to host server IP
 uint16_t port = 8000;
 WiFiClient client;
 
@@ -49,13 +50,12 @@ void setup() {
     Serial.print("WiFi connected. IP: ");
     Serial.println(WiFi.localIP());
 
-    WiFiClient client;
     while(!client.connect(serverHost, port)) {
         Serial.println("Connection to TCP server failed, retrying...");
         delay(1000);
     }
 
-    timer = timerBegin(0, 80, true);
+    timer = timerBegin(0, DIVIDER_VALUE, true);
     timerAttachInterrupt(timer, &onTimer, true);
     timerAlarmWrite(timer, ALARM_VALUE, true);
     timerAlarmEnable(timer);
@@ -66,6 +66,16 @@ void loop() {
     static unsigned long time = micros();
 
     if (bufferFull){
+
+        // Check if still connected
+        if (!client.connected()) {
+            Serial.println("Disconnected from server, reconnecting...");
+            while(!client.connect(serverHost, port)) {
+                Serial.println("Reconnection failed, retrying...");
+                delay(1000);
+            }
+            Serial.println("Reconnected to server!");
+        }
 
         char localBuffer[SAMPLES * 2];
 
@@ -80,7 +90,7 @@ void loop() {
         
         String payload = String((const char*)localBuffer, SAMPLES * 2);
         int length = payload.length();
-        String LengthHeader = "Length:" + String(length + 1) + '\n';
+        String LengthHeader = "Length:" + String(length) + '\n';
         
         // send data via TCP
         client.print(timeHeader);
